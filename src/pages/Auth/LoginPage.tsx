e9562d5ad7cc14bbe5458f9845b822990fe063f7
cliffamadeus <dev.clfm5t12@gmail.com>
2025-04-29 20:50:08 +0800

    LoginPage and index.html changes

--- src/pages/Auth/LoginPage.tsx
+++ src/pages/Auth/LoginPage.tsx
@@ -9,7 +9,7 @@ const LoginPage = () => {
     <Container maxWidth="xs">
       <Paper elevation={3} sx={{ p: 4, mt: 8 }}>
         <Typography variant="h4" component="h1" align="center" gutterBottom>
-          Alumni Portal
+          Project B.E.A.C.O.N
         </Typography>
         <Typography variant="body1" align="center" sx={{ mb: 4 }}>
           Please sign in to continue
